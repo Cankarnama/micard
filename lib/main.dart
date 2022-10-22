14bb33531568e75cfc3dadbe7e4b5a003d480be3
Your Name <you@example.com>
2022-10-22 01:10:30 +0000

    Introduction of Hot reload

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,18 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+   MyApp()
   );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+    home: Scaffold(
+    backgroundColor: Colors.tealAccent,
+body: Container(),
+),
+);
+  }
 }
