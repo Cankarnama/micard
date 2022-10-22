75ac4c90716a45a3fc76062c72247fed3e1e546f
Your Name <you@example.com>
2022-10-22 23:23:58 +0000

    New Widget, Circle Avatar

--- lib/main.dart
+++ lib/main.dart
@@ -12,63 +12,85 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(//FLUTTER LAYOUT CHEAT SHE
-          //This is used to cut off the extra space that the container uses up
-            //mainAxisSize:MainAxisSize.min,
+            child: Column(
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('image/Dearra.jpg'),
+            ),
+            Text(
+                'Dearra Taylor',
 
-          /*This is used to arrange the containers vertically in the opposite direction (up)
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+              color: Colors.deepOrange
+
+
+            ),
+            )
+          ],
+        )),
+      ),
+    );
+  }
+}
+
+/*
+//The different alignments of the column
+
+(//FLUTTER LAYOUT CHEAT SHE
+//This is used to cut off the extra space that the container uses up
+//mainAxisSize:MainAxisSize.min,
+
+/*This is used to arrange the containers vertically in the opposite direction (up)
             down is the default
             */
-            verticalDirection:VerticalDirection.down ,
+verticalDirection:VerticalDirection.down ,
 
-          /*this is used to align the containers based on the main axis of the column
+/*this is used to align the containers based on the main axis of the column
             start ,center and, spaceBetween ,spaceEvenly end
             */
-          //mainAxisAlignment:MainAxisAlignment.spaceBetween ,
+//mainAxisAlignment:MainAxisAlignment.spaceBetween ,
 
-            /* this is used to align the children together ;center, end and start
+/* this is used to align the children together ;center, end and start
 
             */
-            crossAxisAlignment:CrossAxisAlignment.stretch ,
-            children:<Widget> [
-              Container(
-                color: Colors.white,
-                height: 100,
-                /*width: double.infinity,; stretches the container to the end;
+crossAxisAlignment:CrossAxisAlignment.stretch ,
+children:<Widget> [
+Container(
+color: Colors.white,
+height: 100,
+/*width: double.infinity,; stretches the container to the end;
                  but CrossAxisAlignment.stretch , is a more accurate way of doing it if
                  you want to stretch all the containers
 
 
                  */
-                width: 150,
-                child: Text('hello there'),
-              ),
-              SizedBox(
-                height: 50
-              ),
-              Container(
-                color: Colors.brown,
-                height: 100,
-                width: 150,
-                child: Text('hello there'),
-              ),
-              Container(
-                color: Colors.black,
-                height: 100,
-                width: 150,
-                child: Text('hello there'),
-              ),
-              Container(
-                width: double.infinity,
-              )/*
+width: 150,
+child: Text('hello there'),
+),
+SizedBox(
+height: 50
+),
+Container(
+color: Colors.brown,
+height: 100,
+width: 150,
+child: Text('hello there'),
+),
+Container(
+color: Colors.black,
+height: 100,
+width: 150,
+child: Text('hello there'),
+),
+Container(
+width: double.infinity,
+)/*
                 this container was used in order to allow the other containers align to it to the end
                 since its with fills up the who stretch
               */
-            ],
-          )
-        ),
-      ),
-    );
-  }
-}
-//The different alignments of the column
+],
+
+ */
