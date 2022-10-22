e49f916af587ebc4391dc7e0e66c5a0f545da99e
Your Name <you@example.com>
2022-10-22 06:21:20 +0000

    Intro to container, margin , padding and safe area

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-   MyApp()
-  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-    home: Scaffold(
-    backgroundColor: Colors.tealAccent,
-body: Container(),
-),
-);
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.tealAccent,
+        body: SafeArea(
+          child: Container(
+            color: Colors.white,
+            height: 100,
+            width: 150,
+            margin:EdgeInsets.fromLTRB(15, 15, 15, 15),
+            padding: EdgeInsets.all(20),
+            child: Text(
+              'hello there'),
+
+          ),
+        ),
+      ),
+    );
   }
 }
