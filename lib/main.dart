49dd250a82f7b6ec77154b14502fa8a0163c94e8
Your Name <you@example.com>
2022-10-24 09:32:26 +0000

    Card class

--- lib/main.dart
+++ lib/main.dart
@@ -35,52 +35,60 @@ class MyApp extends StatelessWidget {
                     color: Colors.black,
                     fontFamily: 'SourceSansPro'),
               ),
-              Container(
+              Card(
                   color: Colors.black,
                   margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                  padding: EdgeInsets.all(20),
-                  //padding: EdgeInsets.,
+                  //padding: EdgeInsets.all(10),
 
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.phone,
+                          color: Colors.brown[100],
+                          size: 20,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          '+233 504 734 2464',
+                          style:
+                              TextStyle(color: Colors.brown[100], fontSize: 15),
+                        ),
+                      ],
+                    ),
+                  )),
+              Card(
+                /*
+                The container widget can be used in place of the card widget but the card is more convenient
+                since it has some in built properties and automatically comes with some functions
+
+                 */
+
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                //padding: EdgeInsets.all(10),
+                color: Colors.black,
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  //here, padding was used as parent because padding is not one of the properties that come with
+                  //the card class.
                   child: Row(
                     children: <Widget>[
                       Icon(
-                        Icons.phone,
+                        Icons.email,
                         color: Colors.brown[100],
-                        size: 20,
                       ),
                       SizedBox(
                         width: 10,
                       ),
                       Text(
-                        '+233 504 734 2464',
-                        style:
-                            TextStyle(
-                                color: Colors.brown[100],
-                                fontSize: 15),
-                      ),
+                        'Karenamarfio@gmail.com',
+                        style: TextStyle(color: Colors.brown[100], fontSize: 15),
+                      )
                     ],
-                  )),
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                padding: EdgeInsets.all(20),
-                color: Colors.black,
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.email,
-                      color: Colors.brown[100],
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Text(
-                      'Karenamarfio@gmail.com',
-                      style: TextStyle(
-                        color: Colors.brown[100],
-                          fontSize: 15
-                      ),
-                    )
-                  ],
+                  ),
                 ),
               )
             ],
@@ -100,7 +108,7 @@ class MyApp extends StatelessWidget {
 
 /*This is used to arrange the containers vertically in the opposite direction (up)
             down is the default
-            */
+                                                                                                                                                                                                                                                                                                                                                        */
 verticalDirection:VerticalDirection.down ,
 
 /*this is used to align the containers based on the main axis of the column
